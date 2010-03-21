9317c71074cf5875c8c13958e3381872b844dfd4
SND\IceChat_cp <SND\IceChat_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2010-03-21 07:25:57 +0000

    Forgot FormDCCAccept

---
+++ Forms/FormDCCAccept.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IceChat.Forms
+{
+    public partial class FormDCCAccept : Form
+    {
+        public FormDCCAccept()
+        {
+            InitializeComponent();
+        }
+    }
+}
